bc2061a67a22b03f9ca7c5441b28a06999eb7b93
AlexNovRS7 <alexander.nowosad@gmail.com>
2023-11-12 08:33:38 +0100

    auto refreshing token

--- main.py
+++ main.py
@@ -8,15 +8,12 @@ import csv
 import json
 from dateutil import parser
 
-import logging
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
 
 def refresh_token(old_refresh_token):
     response = requests.post(
         url = 'https://www.strava.com/oauth/token',
         data = {'client_id':CLIENT_ID,
-                'client_secret':"9859ded508278dfbcf717854a3636bdb396ed6bb",
+                'client_secret':CLIENT_SECRET,
                 'refresh_token':old_refresh_token,
                 'grant_type':'refresh_token'})
     json_response = json.loads(response.text)
@@ -27,7 +24,8 @@ def refresh_token(old_refresh_token):
         data = {
             "REFRESH_TOKEN": REFRESH_TOKEN,
             "CLIENT_ID": CLIENT_ID,
-            "ACCESS_TOKEN": ACCESS_TOKEN
+            "ACCESS_TOKEN": ACCESS_TOKEN,
+            "CLIENT_SECRET": CLIENT_SECRET
         }
         with open('secrets.json', 'w') as f:
             json.dump(data, f)
@@ -37,7 +35,7 @@ def refresh_token(old_refresh_token):
 
 
 def get_all_cycling_activities(access_token, after):
-    logging.info("started loading activities")
+    print("started loading activities")
     base_url = "https://www.strava.com/api/v3/athlete/activities"
     page = 1
     per_page = 30  # Adjust as needed
@@ -52,7 +50,11 @@ def get_all_cycling_activities(access_token, after):
 
         if response.status_code != 200:
             print(f"Error: {response.status_code}")
-            break
+            try:
+                refresh_token(REFRESH_TOKEN)
+                print("Refreshed token succesfully")
+            except:
+                break
 
         activities = response.json()
 
@@ -87,7 +89,7 @@ def save_to_csv(data, filename):
 
 
 def get_power_stream(activity_id):
-    logging.info(f"Downloading power for {activity_id}")
+    print(f"Downloading power for {activity_id}")
     while True:
         stream_response = session.get(f"https://www.strava.com/api/v3/activities/{activity_id}/streams/watts?access_token={ACCESS_TOKEN}") 
         stream_json = json.loads(stream_response.text)
@@ -114,12 +116,13 @@ with open('secrets.json') as secrets_file:
     CLIENT_ID = parsed_json["CLIENT_ID"]
     REFRESH_TOKEN = parsed_json["REFRESH_TOKEN"]
     ACCESS_TOKEN = parsed_json["ACCESS_TOKEN"]
+    CLIENT_SECRET = parsed_json["CLIENT_SECRET"]
 
 
 # open downloaded activities
 with open('activities.json') as activities_file:
     all_cycling_activities = json.load(activities_file)
-logging.info(f"Opened {len(all_cycling_activities)} existing activities")
+print(f"Opened {len(all_cycling_activities)} existing activities")
 
 
 # check last activity epoch
@@ -128,10 +131,10 @@ last_epoch = max([parser.parse(activity["start_date"]).timestamp() for activity 
 session = requests.Session()
 
 new_cycling_activities = get_all_cycling_activities(ACCESS_TOKEN, last_epoch)
-logging.info(f"Downloaded {len(new_cycling_activities)} new activities")
+print(f"Downloaded {len(new_cycling_activities)} new activities")
 
 all_cycling_activities.extend(new_cycling_activities)
-logging.info(f"Stacked both files. Activities in sum {len(all_cycling_activities)}")
+print(f"Stacked both files. Activities in sum {len(all_cycling_activities)}")
 
 # download power streams
 for activity in all_cycling_activities:
@@ -143,7 +146,7 @@ for activity in all_cycling_activities:
 #save new information to json
 with open('activities.json', 'w') as activities_file:
     json.dump(all_cycling_activities, activities_file)
-logging.info("JSON file updated")
+print("JSON file updated")
 
 df = pd.DataFrame(all_cycling_activities)
 df = df.loc[(df["type"].str.contains("Ride"))]
@@ -153,30 +156,30 @@ df.sort_values("start_date", inplace=True)
 
 ftp_df = pd.read_csv('ATP - FTP.csv')
 ftp_df["Date"] = pd.to_datetime(ftp_df["Date"], format="%d/%m/%Y").dt.date
-logging.info("FTP file loaded")
+print("FTP file loaded")
 
 df = df.merge(ftp_df[["Date", "Power"]], how='left', left_on='start_date', right_on='Date')
 df["FTP set"] = df["Power"].ffill()
 df.drop(["Date", "Power"], axis=1, inplace=True)
-logging.info("Activities and FTP dataframes merged")
+print("Activities and FTP dataframes merged")
 
 df["NP"] = df["power stream"].apply(lambda x: calculate_np(x))
-logging.info("NP calculated")
+print("NP calculated")
 
 df["IF"] = df["NP"]/df["FTP set"]
-logging.info("IF calculated")
+print("IF calculated")
 
 df["TSS"] = ((df["moving_time"] * df["NP"] * df["IF"]) / (df["FTP set"]*3600)) * 100
-logging.info("TSS calculated")
+print("TSS calculated")
 
 df["CTL"] = df["TSS"].rolling(42,0).mean().shift(1)
-logging.info("CTL calculated")
+print("CTL calculated")
 
 df["ATL"] = df["TSS"].rolling(77,0).mean().shift(1)
-logging.info("ATL calculated")
+print("ATL calculated")
 
 df["TSB"] = df["CTL"] - df["ATL"]
-logging.info("TSB calculated")
+print("TSB calculated")
 
 save_to_csv(df, 'cycling_activities.csv')
-logging.info("File Saved")
+print("File Saved")
